a3d5a5aca90843575c4912bfbd26530368c03bec
JarrudV <Jarrud@muloo.co>
2026-01-29 10:02:57 +0200

    Add missing workspace file and update configurations

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 4173,
-    host: '0.0.0.0',
-    allowedHosts: ['.+', 'fampals-925495129376.europe-west4.run.app']
+  server: {
+    host: true,
   },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
+  preview: {
+    host: true,
   }
 });
